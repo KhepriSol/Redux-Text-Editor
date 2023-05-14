75c734539c08785a06018517678069dbf2fc7626
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-05-13 21:08:13 -0500

    database.js logic added
    -created a connection, specifying privileges, opening the store, adding content, and confirming the request.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,8 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//creating a connection, specifying privileges, opening the store, adding content, and confirming the request.
+export const putDb = async (content)  => {
+  console.log('PUT DataBase');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
